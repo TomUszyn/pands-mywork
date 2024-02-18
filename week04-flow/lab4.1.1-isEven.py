8f603be20329369782541e52e193930b7d18e08c
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-02-18 01:37:17 +0000

    week04

--- week04-flow/lab4.1.1-isEven.py
+++ week04-flow/lab4.1.1-isEven.py
@@ -1,4 +1,4 @@
-cd# lab4.1.1-isEven.py
+# lab4.1.1-isEven.py
 # Program asks the user to enter in a number, and the program will tell the user if the number is even or odd.
 # author: Tomasz Uszynski
 
