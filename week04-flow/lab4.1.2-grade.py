8372221d6820a7b604a4fcb631c022fe279d0736
Tomasz Uszynski <tomaszuszynski3@gmail.com>
2024-02-18 01:48:02 +0000

    Add round

--- week04-flow/lab4.1.2-grade.py
+++ week04-flow/lab4.1.2-grade.py
@@ -1,18 +1,20 @@
 # lab4.1.2-grade.py
 # Program reads in a student’s percentage mark and prints out corresponding the grade.
-#author: Tomasz Uszynski
+# author: Tomasz Uszynski
 
 percentage = float(input("Enter the percetage: "))     # Enter float number.
 
-if percentage < 0 or percentage > 100:                 # Checks if we are inside expected range.
+roundedNumber = round(percentage)
+
+if roundedNumber < 0 or roundedNumber > 100:           # Checks if we are inside expected range.
     print("Please enter a number between 0 and 100")   # If not inform to use correct number.
-elif percentage < 40:                                  # Range between 0 and 39.
+elif roundedNumber < 40:                               # Range between 0 and 39.
     print("Fail")                                      # Prints out Fail.
-elif percentage < 50:                                  # Range between 40 and 49.
+elif roundedNumber < 50:                               # Range between 40 and 49.
     print("Pass")                                      # Prints out Pass.
-elif percentage < 60:                                  # Range between 50 and 59.
+elif roundedNumber < 60:                               # Range between 50 and 59.
     print("Merit 2")                                   # Prints out Merit 2.
-elif percentage < 70:                                  # Range between 60 and 69.
+elif roundedNumber < 70:                               # Range between 60 and 69.
     print("Merit 1")                                   # Prints out Merit 1. 
 else:                                                  # Range betwwen 70 and 100.
     print("Distinction")                               # Prints out Distiction. 
